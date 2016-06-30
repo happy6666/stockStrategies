b19111bfc49ba45e83b361f84bb9f975d4c7bd98
yiwen <art.yiwenfu@gmail.com>
2016-06-30 22:19:21 +0800

    Update daily script Add ri jiaoyi script into it

--- options/script/daily_job.sh
+++ options/script/daily_job.sh
@@ -3,11 +3,14 @@ TODAY=$(date --date='1 day ago' +'%Y%m%d')
 DATA_FILE_JIAOYI='../data/all_jiaoyi_data.asv'
 DATA_FILE_JIESUAN='../data/all_jiesuan_data.asv'
 DATA_FILE_HUIZONG_JIESUAN='../data/all_huizongjiesuan_data.asv'
+DATA_FILE_RIJIAOYI='../data/all_rijiaoyi_data.asv'
 LOG_FILE_JIAOYI="../log/${TODAY}_jiaoyi.log"
 LOG_FILE_JIESUAN="../log/${TODAY}_jiesuan.log"
 LOG_FILE_HUIZONG_JIESUAN="../log/${TODAY}_huizongjiesuan.log"
+LOG_FILE_RIJIAOYI="../log/${TODAY}_rijiaoyi.log"
 cd $DIR
 echo $TODAY
 python jiaoyi_parameter.py $TODAY $DATA_FILE_JIAOYI 1>${LOG_FILE_JIAOYI} 2>${LOG_FILE_JIAOYI}
 python jiesuan_parameter.py $TODAY $DATA_FILE_JIESUAN 1>${LOG_FILE_JIESUAN} 2>${LOG_FILE_JIESUAN} 
 python huizongjiesuan_parameter.py $TODAY $DATA_FILE_JIESUAN 1>${LOG_FILE_JIESUAN} 2>${LOG_FILE_JIESUAN} 
+python rijiaoyi_parameter.py $TODAY $DATA_FILE_RIJIAOYI 1>${LOG_FILE_RIJIAOYI} 2>${LOG_FILE_RIJIAOYI} 
