93972592eb8d8c3191a92f1b08d4b1cce5861674
yiwen <art.yiwenfu@gmail.com>
2016-06-30 22:16:43 +0800

    Add daily job script

--- options/script/daily_job.sh
+++ options/script/daily_job.sh
@@ -1,7 +1,13 @@
 DIR=/home/yiwen/stockStrategies/options/script
 TODAY=$(date --date='1 day ago' +'%Y%m%d')
-DATA_FILE='../data/all_jiaoyi_data.asv'
-LOG_FILE="../log/${TODAY}.log"
+DATA_FILE_JIAOYI='../data/all_jiaoyi_data.asv'
+DATA_FILE_JIESUAN='../data/all_jiesuan_data.asv'
+DATA_FILE_HUIZONG_JIESUAN='../data/all_huizongjiesuan_data.asv'
+LOG_FILE_JIAOYI="../log/${TODAY}_jiaoyi.log"
+LOG_FILE_JIESUAN="../log/${TODAY}_jiesuan.log"
+LOG_FILE_HUIZONG_JIESUAN="../log/${TODAY}_huizongjiesuan.log"
 cd $DIR
 echo $TODAY
-python jiaoyi_parameter.py $TODAY $DATA_FILE 1>${LOG_FILE} 2>${LOG_FILE}
+python jiaoyi_parameter.py $TODAY $DATA_FILE_JIAOYI 1>${LOG_FILE_JIAOYI} 2>${LOG_FILE_JIAOYI}
+python jiesuan_parameter.py $TODAY $DATA_FILE_JIESUAN 1>${LOG_FILE_JIESUAN} 2>${LOG_FILE_JIESUAN} 
+python huizongjiesuan_parameter.py $TODAY $DATA_FILE_JIESUAN 1>${LOG_FILE_JIESUAN} 2>${LOG_FILE_JIESUAN} 
