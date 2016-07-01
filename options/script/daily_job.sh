7818019f07a398b18a8813bfcf4ff5347b57ad66
happy6666 <art.yiwenfu@gmail.com>
2016-07-01 10:44:11 +0800

    Update and fix bug

--- options/script/daily_job.sh
+++ options/script/daily_job.sh
@@ -6,16 +6,21 @@ DATA_FILE_JIESUAN='../data/all_jiesuan_data.asv'
 DATA_FILE_HUIZONG_JIESUAN='../data/all_huizongjiesuan_data.asv'
 DATA_FILE_RIJIAOYI='../data/all_rijiaoyi_data.asv'
 DATA_FILE_YEWU='../data/all_yewu_data.asv'
+DATA_FILE_DALIAN='../data/all_dalian_tongji_data.asv'
 
 LOG_FILE_JIAOYI="../log/${TODAY}_jiaoyi.log"
 LOG_FILE_JIESUAN="../log/${TODAY}_jiesuan.log"
 LOG_FILE_HUIZONG_JIESUAN="../log/${TODAY}_huizongjiesuan.log"
 LOG_FILE_RIJIAOYI="../log/${TODAY}_rijiaoyi.log"
 LOG_FILE_YEWU="../log/${TODAY}_yewu.log"
+LOG_FILE_DALIAN='../log/${TODAY}_daliantongji.log'
+
 cd $DIR
 echo $TODAY
+
 python jiaoyi_parameter.py $TODAY $DATA_FILE_JIAOYI 1>${LOG_FILE_JIAOYI} 2>${LOG_FILE_JIAOYI}
 python jiesuan_parameter.py $TODAY $DATA_FILE_JIESUAN 1>${LOG_FILE_JIESUAN} 2>${LOG_FILE_JIESUAN} 
-python huizongjiesuan_parameter.py $TODAY $DATA_FILE_JIESUAN 1>${LOG_FILE_JIESUAN} 2>${LOG_FILE_JIESUAN} 
+python huizongjiesuan_parameter.py $TODAY $DATA_FILE_HUIZONG_JIESUAN 1>${LOG_FILE_HUIZONG_JIESUAN} 2>${LOG_FILE_HUIZONG_JIESUAN} 
 python rijiaoyi_parameter.py $TODAY $DATA_FILE_RIJIAOYI 1>${LOG_FILE_RIJIAOYI} 2>${LOG_FILE_RIJIAOYI} 
 python yewu_parameter.py $TODAY $DATA_FILE_YEWU 1>${LOG_FILE_YEWU} 2>${LOG_FILE_YEWU} 
+python dalian_tongji_parameter.py $TODAY $DATA_FILE_DALIAN 1>${LOG_FILE_DALIAN} 2>${LOG_FILE_DALIAN} 
